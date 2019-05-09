58eabdcc3568546a60d3e29ed1a279600a60cba2
shuo heng Wei <hcoolg@gmail.com>
2019-05-09 03:46:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,7 +8,7 @@ function basicTeenager(age) {
 }
 function teenager(age) {
     if ('the age is not between 13- 19'){
-    return 'You are not a teenager'
+    return 'You are a teenager'
   }
 }
 function ageChecker(age) {
