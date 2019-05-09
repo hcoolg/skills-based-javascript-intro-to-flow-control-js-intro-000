7e8f1bea053e653c134a1c9fd30be47bf158eeaa
shuo heng Wei <hcoolg@gmail.com>
2019-05-09 04:54:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,8 +28,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   case "13, 14, 15, 16, 17, 18, 19":
-    
-  return"You are a teenager'
+    return 'You are a teenager'
   default:
   return 'You have an age'
 }
